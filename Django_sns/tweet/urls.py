b84681b2f8eee20fcd4438ef1a2208b4a6b9ac31
Carrot_hyeong <104682050+Carrotww@users.noreply.github.com>
2022-09-26 14:38:47 +0900

    commit

--- Django_sns/tweet/urls.py
+++ Django_sns/tweet/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('tweet/', views.tweet, name='tweet'), # 127.0.0.1:8000/tweet 과 views.py 폴더의 tweet 함수 연결
     # tweet 함수로 접근을 한다면 views 안에 tweet 함수를 실행
     path('tweet/delete/<int:id>', views.delete_tweet, name='delete-tweet'),
+    path('tweet/<int:id>', views.detail_tweet, name='detail-tweet'),
+    path('tweet/comment/<int:id>', views.write_comment, name='write-comment'),
+    path('tweet/comment/delete/<int:id>', views.delete_comment, name='delete-comment'),
 ]
